95e9d5ed4581fd1d110ccdadbc32200115c68546
Robert Brown Jr <Robert.BrownJr.Dev@gmail.com>
2023-06-14 19:29:30 -0400

    Filled in method signatures for AccountDao interface

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,20 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Transfer;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface AccountDao {
+    List<Transfer> findTransfersByUserId(int userId);
+
+    List<Transfer> findPendingTransfersByUserId(int userId);
+
+    BigDecimal findBalanceByUserId(int userId);
+
+    Transfer findTransferByTransferId(int transferId);
+
+    Transfer saveTransfer(Transfer transfer);
+
+    void updateTransferStatus(Transfer transfer);
 }
